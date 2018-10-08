7f7bc2081284bb9f9926e0c3e723ee9c195ad75e
Jan Vandepitte <Jan.Vandepitte@sirus.be>
2018-10-08 17:51:30 +0200

    second equation

--- Module01/excercises.py
+++ Module01/excercises.py
@@ -10,3 +10,15 @@ y = eqsy[2]/eqsy[1]
 eqsx = eqs[0]
 print(eqsx)
 print((eqsx[2]-(eqsx[1]*y))/eqsx[0])
+
+eqs = np.array([[2,3,23],[1,4,24]])
+print(eqs)
+print(eqs[1]*2)
+eqs[1] = eqs[1]*2
+print(eqs[0]- eqs[1])
+eqsy = eqs[0]- eqs[1]
+print(eqsy[2]/eqsy[1])
+y = eqsy[2]/eqsy[1]
+eqsx = eqs[0]
+print(eqsx)
+print((eqsx[2]-(eqsx[1]*y))/eqsx[0])
