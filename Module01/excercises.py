1e8c3b82c2dc88ff95a59623b15e108bfb1503a2
Jan Vandepitte <Jan.Vandepitte@sirus.be>
2018-10-08 15:33:21 +0200

    system of equations

---
+++ Module01/excercises.py
@@ -0,0 +1,12 @@
+import numpy as np
+eqs = np.array([[2,1,13],[10,-2,44]])
+print(eqs)
+print(eqs[0]*5)
+eqs[0] = eqs[0]*5
+print(eqs[0]- eqs[1])
+eqsy = eqs[0]- eqs[1]
+print(eqsy[2]/eqsy[1])
+y = eqsy[2]/eqsy[1]
+eqsx = eqs[0]
+print(eqsx)
+print((eqsx[2]-(eqsx[1]*y))/eqsx[0])
