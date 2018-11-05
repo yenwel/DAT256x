6b15a05f84f6203490f72d14e52dcaf336ae1986
Jan Vandepitte <Jan.Vandepitte@sirus.be>
2018-11-05 21:31:27 +0100

    differentiation

---
+++ Module02/excercises.py
@@ -0,0 +1,29 @@
+def dx(x):
+    return (x**2) + (2*x)
+print(dx(5))
+print(dx(1))
+
+x1 = 2
+x2 = 5
+y1 = dx(x1)
+y2 = dx(x2)
+rate = (y1-y2) / (x1-x2)
+print(rate)
+
+def f(x):
+    return (2*(x**2))/(x-2)
+print(f(1))
+#print(f(2))
+
+print(-12/6)
+
+def f1(x):
+    return ((2*(x**2))-3)/(x+1)
+print(f1(0))
+
+def df2(x):
+    return (8*(x**3)) - (6*x) + 2
+
+print(df2(-1))
+
+print(30/4)
